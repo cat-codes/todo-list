2390cab2292421b54f45fcfa294cdaca5f4d70e5
Milda <milda.singh@gmail.com>
2024-01-23 02:28:46 +0100

    typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ContextTheme } from './components/contextTheme.jsx'
+import { ContextTheme } from './components/ContextTheme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextTheme>
